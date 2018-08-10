ff89baa395e54f5eae24e240a5055ef0e7290467
bhartendush <bhartendu7285@gmail.com>
2018-08-10 12:06:34 +0530

    added config folder

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // configure the code below with your username, password and mlab database information
+    database: 'mongodb://c19:connect19@ds215502.mlab.com:15502/connect19',   //prod
+    //database: 'mongodb://localhost:27017/connect19'    //dev
+  }
