6b538eae661c012a7c20e83b1240965eef9259e2
bhartendush <bhartendu7285@gmail.com>
2018-08-10 12:14:45 +0530

    updated connection string#2

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     // configure the code below with your username, password and mlab database information
-    database: 'mongodb://connect19:C19@#$%@1245@ds215502.mlab.com:15502/connect19',  //prod
+    database: 'mongodb://c19:connect19@ds215502.mlab.com:15502/connect19',  //prod
     //database: 'mongodb://localhost:27017/connect19'    //dev
   }
