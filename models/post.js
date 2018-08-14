ab80a0147ed731d48654e7475ba92bef0a71111e
bhartendush <bhartendu7285@gmail.com>
2018-08-14 22:39:37 +0530

    added models

---
+++ models/post.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+// Posts Schema
+var PostSchema = mongoose.Schema({	
+	description: {
+		type: String
+    },
+    postimage:{
+        type: String
+	},
+	author: {
+		type: String
+	},
+	date: {
+		type: Date
+	}
+});
+
+var Post = module.exports = mongoose.model('Post', PostSchema);
+
+module.exports.createPost = function(newPost, callback){
+	        newPost.save(callback);	
+}
