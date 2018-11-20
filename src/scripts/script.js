ffc5ff83928ac92c87009b7ced394d55b0746d41
bhartendush <bhartendu7285@gmail.com>
2018-11-20 10:09:46 +0530

    Fixed chat functionality

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -351,35 +351,7 @@
 
     
 
-    
-// Script for popup for groups
-     // Get the modal 
-var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
-// Script for popup for groups ends here
 
     // Chat between users
     $('.chat_button').on("click", function(){
@@ -493,3 +465,7 @@ function filterUser() {
         }       
       }
   }
+
+
+
+  
