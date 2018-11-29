82a5e48c3dc0a2e7ed4bef0a5b346d32e9ed9a6a
Raven Duran <rabintoy@gmail.com>
2018-11-29 18:36:27 +0800

    Modified Users API

--- routes/api/users.js
+++ routes/api/users.js
@@ -31,12 +31,31 @@ router.get('/username', function (req, res) {
         res.status(404).json({ error: "Username Does not Exists" });
         return;
     }
-    User.find({ "username": { $ne: req.query.username } }, { "password": 0, "group_invitation": 0, "friend_requests": 0 }, function (err, users) {
-        if (err) throw err;
-        var userData = {
-            users: users
+
+    User.findOne({ username: req.query.username  }, { "password": 0, "group_invitation": 0, "friend_requests": 0 }, function (err, user) {
+        if (err) {
+            res.status(500).send(JSON.stringify({ success: false, msg: "unable to get user from username" }));
+        } else {
+            res.send(JSON.stringify({ success: true, user: user }));
         }
-        res.send(JSON.stringify({ user: userData }));
+        
+        //res.render('friends/users', {user_friends: users, users: user, isApproved: user.isApproved, user_id: user.member_id, friend_requests: user.friend_requests, friends: user.friends});	
+    });
+});
+
+router.get('/id', function (req, res) {
+    if (!req.query.member_id) {
+        res.status(404).json({ error: "Member Id Does not Exists" });
+        return;
+    }
+
+    User.findOne({ member_id: req.query.member_id }, { "password": 0, "group_invitation": 0, "friend_requests": 0 }, function (err, user) {
+        if (err) {
+            res.status(500).send(JSON.stringify({ success: false, msg: "unable to get user from member_id" }));
+        } else {
+            res.send(JSON.stringify({ success: true, user: user }));
+        }
+        
         //res.render('friends/users', {user_friends: users, users: user, isApproved: user.isApproved, user_id: user.member_id, friend_requests: user.friend_requests, friends: user.friends});	
     });
 });
@@ -82,6 +101,35 @@ router.post('/signin', function (req, res) {
                     res.status(401).send({ success: false, msg: 'Authentication failed. Wrong password.' });
                 }
             });
+        }
+    });
+});
+
+// Update profile
+router.post('/editprofile', authenticateFirst, function (req, res) {
+    var username = req.body.username;
+    var description = req.body.description;
+    var profilePicUrl = req.body.profilePicUrl;
+
+    req.user.user_profile[0].description = description;
+
+    if (!username || username.trim() == "") {
+        res.status(500).json({ success: false, msg: 'Username must be not be empty.' });
+        return;
+    }
+
+    if (profilePicUrl) {
+        req.user.user_profile[0].profilepic = profilePicUrl;
+    }
+
+    User.updateOne({ username: req.user.username }, { $set: { "username": username, "user_profile": req.user.user_profile } }, function (err, user) {
+        //console.log(user);
+        if (err) {
+            res.status(500).json({ success: false, msg: 'Error Updating User.' });
+            return;
+        } else {
+            res.json({ success: true, user: user });
+            return;
         }
     });
 });
