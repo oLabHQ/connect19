8086b87a72a0ff5d170e96001f0c11af2d7c35ea
Raven Duran <rabintoy@gmail.com>
2018-11-30 05:44:10 +0800

    Added API

--- routes/api/friends.js
+++ routes/api/friends.js
@@ -17,56 +17,73 @@ var User = require('../../models/user');
 // 		});	
 // });
 
-router.get('/', authenticateFirst, function(req, res){
-	User.aggregate([{$unwind: "$friends"},{$lookup:{from:"users",localField:"friends", foreignField:"member_id", as:"user_details"}},{$match:{member_id: req.user.member_id}},{$project:{"user_details.isApproved": 1, "user_details.username": 1,"user_details.member_id": 1, "user_details.user_profile.profilepic": 1, "username":1, "member_id":1}}]).exec(function(err, friends){                
-        if(err) {
+router.get('/', authenticateFirst, function (req, res) {
+    User.aggregate([{ $unwind: "$friends" }, { $lookup: { from: "users", localField: "friends", foreignField: "member_id", as: "user_details" } }, { $match: { member_id: req.user.member_id } }, { $project: { "user_details.isApproved": 1, "user_details.username": 1, "user_details.member_id": 1, "user_details.user_profile.profilepic": 1, "username": 1, "member_id": 1 } }]).exec(function (err, friends) {
+        if (err) {
             res.status(500).send(JSON.stringify({ success: false, msg: "Error Getting Friends." }));
         } else {
             res.send(JSON.stringify({ success: true, friends: friends }));
         }
-		//res.render('friends/index',{user:users, users: user, isApproved: user.isApproved})
-		});	
+        //res.render('friends/index',{user:users, users: user, isApproved: user.isApproved})
+    });
 });
 
 
 
 // Post friend Request
-router.post('/send-friend-request', authenticateFirst, function(req, res){
+router.post('/send-friend-request', authenticateFirst, function (req, res) {
     var sender_user_id = req.user.member_id;
     var receiver_user_id = req.body.receiver_member_id;
 
-	User.findOne({"member_id": sender_user_id}, function(err, sending_user){ 
-		if(err) throw err;		
-		User.findOne({"member_id": receiver_user_id}, function(err, potential_friend){
-		//	console.log(potential_friend);
-			if(err) throw err;			
-			
-			potential_friend.update({$push: {"friend_requests": sending_user.member_id}}).exec(function(err, friend_requests){
+    User.findOne({ "member_id": sender_user_id }, function (err, sending_user) {
+        if (err) throw err;
+        User.findOne({ "member_id": receiver_user_id }, function (err, potential_friend) {
+            //	console.log(potential_friend);
+            if (err) throw err;
+
+            potential_friend.update({ $push: { "friend_requests": sending_user.member_id } }).exec(function (err, friend_requests) {
                 //console.log(friend_requests);
                 res.send(JSON.stringify({ friendrequests: friend_requests }));
-            })				
-		});
-	});
+            })
+        });
+    });
 });
 
 // Pending Requests
-router.get('/friend-requests', function(req, res){
-	User.findOne({member_id:req.query.member_id}, function(err, user){        
-        var user_id = req.query.member_id;    
-	//User.findOne({username:req.user.username}, function(err, friendrequests){	
-	User.aggregate([{$unwind: "$friend_requests"},{$lookup:{from:"users",localField:"friend_requests", foreignField:"member_id", as:"user_details"}},{$match:{member_id:user_id}},{$project:{"user_details.username":1, "user_details.member_id":1, "user_details.user_profile":1, "user_details.isApproved":1, }}]).exec(function(err, friendrequests){		
-        if(err) throw err;
-        var friendrequests = {
-            friendrequests: friendrequests,
-            isApproved:user.isApproved
-        }
-        res.send(JSON.stringify({ friendrequests: friendrequests }));
-		//res.render('friends/pendingrequests', {friendrequests: friendrequests.friend_requests});
-		//res.render('friends/pendingrequests', {friendrequests: friendrequests, users: user, isApproved: user.isApproved});
-	});
-});
+// On Accept Friend Request should delete friend request for both records
+router.get('/friend-requests', authenticateFirst, function (req, res) {
+    User.findOne({ member_id: req.user.member_id }, function (err, user) {
+        //User.findOne({username:req.user.username}, function(err, friendrequests){	
+        User.aggregate([{ $unwind: "$friend_requests" }, { $lookup: { from: "users", localField: "friend_requests", foreignField: "member_id", as: "user_details" } }, { $match: { member_id: req.user.member_id } }, { $project: { "user_details.username": 1, "user_details.member_id": 1, "user_details.user_profile": 1, "user_details.isApproved": 1, } }]).exec(function (err, friendrequests) {
+            if (err) {
+                res.status(500).send({ success: false, msg: "Unable to retrieve friend requests. Please try again" });
+            } else {
+                res.send(JSON.stringify({ success: true, friendrequests: friendrequests }));
+            }
+            //res.render('friends/pendingrequests', {friendrequests: friendrequests.friend_requests});
+            //res.render('friends/pendingrequests', {friendrequests: friendrequests, users: user, isApproved: user.isApproved});
+        });
+    });
 });
 
+router.post('/accept-friend-request', authenticateFirst, function (req, res) {
+    User.find({ 'member_id': req.user.member_id }, function (err, user) {
+        //console.log(user);
+        User.find({ "member_id": req.body.other_member_id }, function (err, accepted_friend_user) {
+            user[0].update({ $push: { friends: accepted_friend_user[0].member_id }, $pull: { "friend_requests": req.body.other_member_id } }, function (err) {
+                accepted_friend_user[0].update({ $push: { friends: user[0].member_id } }, function (err) {
+                    if (err) {
+                        res.status(500).send({success: false, msg: "Unable to accept friend request."});
+                        return;
+                    } else {
+                        res.json({success: true});
+                        return
+                    }
+                });
+            });
+        });
+    });
+});
 
 
 module.exports = router;
