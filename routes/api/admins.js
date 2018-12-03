c5dcb3c740a1ec99d2809bf5a6f2c538994d1ae9
Raven Duran <rabintoy@gmail.com>
2018-12-03 15:01:16 +0800

    Revised Admin API

--- routes/api/admins.js
+++ routes/api/admins.js
@@ -6,17 +6,14 @@ var User = require('../../models/user');
 
 
 // Get Users
-router.get('/',authenticateFirst, function(req,res){    
-	User.findOne({"member_id": req.user.member_id}, function(err, user){
-		User.find({"member_id": {$ne: req.user.member_id}},{"username":1, "member_id":1, "user_profile.profilepic":1}, function(err, users){	 
-			if(err) throw err;	
-				console.log(user.admin);				
-			var users = {
-				users: users,
-				username: user.username,
-				isAdmin: user.admin, 
-				isApproved: user.isApproved	
-			}
+router.get('/', authenticateFirst, function (req, res) {
+	if (!req.user.admin) {
+		res.status(403).send({ success: false, msg: "You are not allowed to access this functionality." });
+		return;
+	}
+
+	User.findOne({ "member_id": req.user.member_id }, function (err, user) {
+		User.find({ "member_id": { $ne: req.user.member_id } }, { "username": 1, "member_id": 1, "user_profile.profilepic": 1, "user_profile.description": 1, "isApproved": 1, "admin": 1 }, function (err, users) {
 			if (err) {
 				res.status(500).send(JSON.stringify({ success: false, msg: "Error Getting Users." }));
 			} else {
@@ -24,57 +21,106 @@ router.get('/',authenticateFirst, function(req,res){
 			}
 		});
 	});
- });
+});
 
 
-  // Make admin
-router.post('/', authenticateFirst, function(req, res){
-	User.findOne({'member_id': req.body.member_id}, function(err, user){
-		var isAdmin = req.body.isAdmin;		
-			user.update({$set:{admin: isAdmin}}, function(err){
-				if (err) {
-					res.status(500).send({success: false, msg: "Unable to make admin."});
-					return;
-				} else {
-					res.send({ success: true });
-					return;
-				}
-		});  
+// Make admin
+router.post('/', authenticateFirst, function (req, res) {
+	if (!req.user.admin) {
+		res.status(403).send({ success: false, msg: "You are not allowed to access this functionality." });
+		return;
+	}
+
+	User.findOne({ 'member_id': req.body.member_id }, function (err, user) {
+		user.update({ $set: { admin: true } }, function (err) {
+			if (err) {
+				res.status(500).send({ success: false, msg: "Unable to make admin." });
+				return;
+			} else {
+				res.send({ success: true });
+				return;
+			}
+		});
 	});
-}); 
+});
+
+router.post('/revoke-admin', authenticateFirst, function (req, res) {
+	if (!req.user.admin) {
+		res.status(403).send({ success: false, msg: "You are not allowed to access this functionality." });
+		return;
+	}
+
+	User.findOne({ 'member_id': req.body.member_id }, function (err, user) {
+		user.update({ $set: { admin: false } }, function (err) {
+			if (err) {
+				res.status(500).send({ success: false, msg: "Unable to revoke admin." });
+				return;
+			} else {
+				res.send({ success: true });
+				return;
+			}
+		});
+	});
+});
 
 
 // Approve User
-router.post('/approve', authenticateFirst, function(req, res){
-	User.findOne({'member_id': req.body.member_id}, function(err, user){				
-		var isApproved = req.body.isApproved;
-			user.update({$set:{isApproved: isApproved}}, function(err){
-				if (err) {
-					res.status(500).send({success: false, msg: "Unable to Approve user."});
-					return;
-				} else {
-					res.send({ success: true});
-					return;
-				}
-		});  
+router.post('/approve', authenticateFirst, function (req, res) {
+	if (!req.user.admin) {
+		res.status(403).send({ success: false, msg: "You are not allowed to access this functionality." });
+		return;
+	}
+
+	User.findOne({ 'member_id': req.body.member_id }, function (err, user) {
+		user.update({ $set: { isApproved: true } }, function (err) {
+			if (err) {
+				res.status(500).send({ success: false, msg: "Unable to Approve user." });
+				return;
+			} else {
+				res.send({ success: true });
+				return;
+			}
+		});
+	});
+});
+
+// Revoke User Approval
+router.post('/revoke-approval', authenticateFirst, function (req, res) {
+	if (!req.user.admin) {
+		res.status(403).send({ success: false, msg: "You are not allowed to access this functionality." });
+		return;
+	}
+
+	User.findOne({ 'member_id': req.body.member_id }, function (err, user) {
+		user.update({ $set: { isApproved: false } }, function (err) {
+			if (err) {
+				res.status(500).send({ success: false, msg: "Unable to revoke user approval." });
+				return;
+			} else {
+				res.send({ success: true });
+				return;
+			}
+		});
 	});
 });
 
 
 // Delete User
-router.post('/delete-user', function(req, res){	
-	User.remove({'member_id': req.body.member_id}, function(err, user){
-		console.log(req.body.member_id);
-		if(err) throw err;
+router.delete('/delete-user', authenticateFirst, function (req, res) {
+	if (!req.user.admin) {
+		res.status(403).send({ success: false, msg: "You are not allowed to access this functionality." });
+		return;
+	}
+
+	User.remove({ 'member_id': req.query.member_id }, function (err, user) {
 		if (err) {
-			res.status(500).send({success: false, msg: "Unable to Delete user."});
+			res.status(500).send({ success: false, msg: "Unable to Delete user." });
 			return;
 		} else {
-			res.send({ success: true});
+			res.send({ success: true });
 			return;
 		}
-});
+	});
 });
 
- module.exports = router;
- 
+module.exports = router;
