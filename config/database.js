c1267f655e5acd1ba92eb8881f7012beea581126
Raven Duran <rabintoy@gmail.com>
2018-12-03 15:47:47 +0800

    Edited Production Database

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     // configure the code below with your username, password and mlab database information
     secret: 'connect19APIsecret',
-    database: 'mongodb://c19:connect19@ds215502.mlab.com:15502/connect19',  // staging
-    // database: 'mongodb://connect19:Conn@ct#&^43&@ds123444.mlab.com:23444/connect19' // prod
+    // database: 'mongodb://c19:connect19@ds215502.mlab.com:15502/connect19',  // staging
+    database: 'mongodb://c19:connectnineteen19@ds123444.mlab.com:23444/connect19' // prod
     //database: 'mongodb://localhost:27017/connect19'    //dev
   }
